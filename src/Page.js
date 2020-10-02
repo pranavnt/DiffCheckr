33d003a5c9489903dbfeabc6ba07f5941d889c94
Pranav Teegavarapu <pranavnt@outlook.com>
2020-10-02 11:45:20 -0700

    handling user input works

--- src/Page.js
+++ src/Page.js
@@ -5,24 +5,39 @@ import { Diff } from "../node_modules/diff-sequences";
 export default class Page extends React.Component {
   compareOutputs = (event) => {
     event.preventDefault();
-    alert(this.input.value);
+    const yourCode = this.userInput.value;
+    const correctCode = this.correctOutput.value;
+
+    alert(yourCode);
   };
 
   render() {
     return (
-      <div style={{ height: "100vh", width: "50%", float: left }}>
-        <form onSubmit={this.compareOutputs}>
-          <label htmlFor="username">Your Input:</label>
-          <br />
-          <input
-            type="textarea"
-            name="userInput"
-            defaultValue="Hi!"
-            ref={(input) => (this.input = input)}
-            style={{ width: "100%", height: "45vh" }}
-          />
-        </form>
-      </div>
+      <form
+        onSubmit={this.compareOutputs}
+        style={{ padding: "10px", height: "100vh", width: "100%" }}
+      >
+        <label>Your Input:</label>
+        <br />
+        <input
+          type="textarea"
+          name="userInput"
+          defaultValue="Hi!"
+          ref={(input) => (this.userInput = input)}
+          style={{ width: "47%", height: "100vh", float: "left" }}
+        />
+
+        <label>Correct Output:</label>
+        <br />
+        <input
+          type="textarea"
+          name="correctOutput"
+          defaultValue="Enter the Correct Output here"
+          ref={(input) => (this.correctOutput = input)}
+          style={{ width: "47%", height: "100vh", float: "left" }}
+        />
+        <input type="submit" />
+      </form>
     );
   }
 }
