a7282ed22f0fb5f2f8fc01e742898617abace837
Pranav Teegavarapu <pranavnt@outlook.com>
2020-10-02 19:10:34 -0700

    fixed it ig

--- src/OutputViewer.js
+++ src/OutputViewer.js
@@ -1,9 +1,5 @@
 import React from "react";
-import ReactHtmlParser, {
-  processNodes,
-  convertNodeToElement,
-  htmlparser2,
-} from "react-html-parser";
+import ReactHtmlParser from "react-html-parser";
 
 export default function OutputViewer(props) {
   return <div>{ReactHtmlParser(props.html)}</div>;
